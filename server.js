f7ff862bd28fc120385d4aa4b1210072a6a3fabb
jhondesignee <jhonata.oliveira.275@gmail.com>
2023-01-26 21:23:05 -0300

    update

--- server.js
+++ server.js
@@ -14,36 +14,31 @@ app.use(cors())
 app.use(express.json())
 app.post("/", async (request, response) => {
   const data = request.body
-  if (/^\/ask/i.test(data?.senderMessage ?? "")) {
-    const prompt = data.senderMessage.replace("/ask", "").trim()
-    try {
-      const completion = await openai.createCompletion({
-        model: "text-davinci-003",
-        max_tokens: 4000,
-        prompt
-      })
-      const result = completion.data.choices[0].text
-      if (result) {
-        response.send({
-          data: [{
-            message: result
-          }]
-        })
-      } else {
-        response.end() 
-      }
-    } catch(error) {
-      if (error.response) {
-        console.log(error.response.data)
-      } else {
-        console.log(error.message)
-      }
-      response.status(500).end()
-    }
-  } else {
-    response.end()
+  const isCommand = /^\/ask/i.test(data?.senderMessage ?? "")
+  if (!isCommand) {
+    return response.end()
+  }
+  const prompt = data.senderMessage.replace("/ask", "").trim()
+  if (!prompt) {
+    return response.send({
+      data: [{ message: "Insira uma pergunta após o comando\n\nex.: /ask como resolver equação de segundo grau?" }]
+    })
+  } 
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      max_tokens: 4000,
+      prompt
+    })
+    const result = completion.data.choices[0].text
+    response.send({
+      data: [{ message: result }]
+    })
+  } catch(error) {
+    console.log(error.response.data ?? error.message)
+    response.status(500).end()
   }
 })
 app.listen(process.env.PORT ?? 3000, () => {
-  console.log("Server running")
+  console.log("Bot running")
 })
